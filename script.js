aae0b9e1bd30c0f110985a3bc5eeba8c539d75ae
Jeffrey Jackson <main.jjackson@gmail.com>
2023-08-15 22:11:35 -0500

    add rock paper scissor collisions & sound

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
-import bubble from "./sprite.js";
+import { base, gamepiece } from "./sprite.js";
 
 const board = document.getElementById("board");
 const ctx = board.getContext("2d");
 let bubbles = [];
+let players = [];
 
 
 
 function setup(){
 
-    for (let i = 0; i < 10; i++){
-        bubbles[i] = new bubble((Math.random()*board.width),(Math.random()*board.height));
-        console.log(bubbles)
+    for (let i = 0; i < 20; i++){
+        //bubbles[i] = new base((Math.random()*board.width),(Math.random()*board.height));
+
+
+        players[i] = new gamepiece((Math.random()*board.width),(Math.random()*board.height),'rock');
+        players[i].piece = players[i].pieces[Math.round(Math.random()*(players[i].pieces.length-1))];//
+        players[i].pieceatt();
+
     }
+    console.log(bubbles);
+    console.log(players);
 }
 
 function gameloop(){
     ctx.fillStyle = "black";
     ctx.fillRect(0,0,board.width,board.height);
-    for (let i = 0; i < bubbles.length-1; i++){
+   /* for (let i = 0; i < bubbles.length; i++){
         bubbles[i].draw(ctx);
         bubbles[i].randommovement();
         bubbles[i].edge(board);
-        for(let j=i+1;j<bubbles.length-1;j++){
+        for(let j=i+1;j<bubbles.length;j++){
             bubbles[i].intersect(bubbles[j]);
         }
+        bubbles[i].edge(board);
+        bubbles[i].intersect(players[0]);
+    }*/
+    for (let i = 0; i < players.length; i++){
+        players[i].draw(ctx);
+        players[i].randommovement();
+        players[i].edge(board);
+        for(let j=0;j<players.length;j++){
+            players[i].intersect(players[j]);
+        }
     }
+
+
 
 }
 
--- sprite.js
+++ sprite.js
@@ -1,21 +1,19 @@
-export default class bubble{
+export class base{
     constructor(x,y){
         this.x = x;
         this.y = y;
         this.width = 50;
         this.height = 50;
         this.color = 'rgb('+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+')';
-        this.bump = new Audio('./stone.mp3');
+        this.sound = new Audio("");
     }
-    
     randommovement(){
-        this.x += (Math.round(Math.random()*10));
-        this.x -= (Math.round(Math.random()*10));
-        this.y += (Math.round(Math.random()*10));
-        this.y -= (Math.round(Math.random()*10));
+        this.x += (Math.round(Math.random()*15));
+        this.x -= (Math.round(Math.random()*15));
+        this.y += (Math.round(Math.random()*15));
+        this.y -= (Math.round(Math.random()*15));
     }
     draw(ctx){
-        //ctx.fillRect(this.x-(this.width/2),this.y-(this.width/2),this.x+(this.width/2),this.y+(this.width/2))
         ctx.strokeStyle ="white";
         ctx.fillStyle=this.color;
         ctx.strokeRect(this.x-25,this.y-25,this.width,this.height);
@@ -28,11 +26,56 @@ export default class bubble{
         }
     }
     edge(board){
-        if(this.x < this.width/2){this.x =this.width/2; this.bump.play();};
-        if(this.x > board.width-(this.width/2)) {this.x = board.width-(this.width/2); this.bump.play();};
-        if(this.y < this.height/2){this.y=this.height/2; this.bump.play();};
-        if(this.y > board.height-(this.height/2)){this.y = board.height-(this.height/2); this.bump.play();};
+        if(this.x < this.width/2){this.x =this.width/2};
+        if(this.x > board.width-(this.width/2)) {this.x = board.width-(this.width/2)};
+        if(this.y < this.height/2){this.y=this.height/2}
+        if(this.y > board.height-(this.height/2)){this.y = board.height-(this.height/2)};
     }
 
     }
+
+export class gamepiece{
+    constructor(x,y,piece){
+        this.x = x;
+        this.y = y;
+        this.piece=piece;
+        this.width = 50;
+        this.height = 50;
+        this.image = new Image(this.width,this.height);
+        this.sound = new Audio("");
+        this.sounds = [new Audio('./stone.mp3'),new Audio('./crumbling_paper.mp3'),new Audio('./scissors-a.mp3')];
+        this.pieces=['rock','paper','scissors'];
+
+    }
+    pieceatt(){
+        if (this.piece=='rock'){this.image.src = './rock.png';this.sound=this.sounds[0]};
+        if (this.piece=='paper'){this.image.src = './paper.png';this.sound=this.sounds[1]};
+        if (this.piece=='scissors'){this.image.src = './scissor.png';this.sound=this.sounds[2]};
+    }
+
+    
+    draw(ctx){
+        ctx.drawImage(this.image,this.x-25,this.y-25,this.width,this.height);
+    }
+    intersect(other){
+        if (Math.abs(this.x-other.x) < 50  &&  Math.abs(this.y-other.y) < (this.height+other.height)/2){
+            if(this.piece == 'rock' && other.piece == 'scissors'){other.piece = 'rock'; other.pieceatt(); this.sound.play();};
+            if(this.piece == 'scissors' && other.piece == 'paper'){other.piece = 'scissors'; other.pieceatt(); this.sound.play();};
+            if(this.piece == 'paper' && other.piece == 'rock'){other.piece = 'paper'; other.pieceatt(); this.sound.play();};
+        }
+    }
+    randommovement(){
+        this.x += (Math.round(Math.random()*30));
+        this.x -= (Math.round(Math.random()*30));
+        this.y += (Math.round(Math.random()*30));
+        this.y -= (Math.round(Math.random()*30));
+    }
+    convert(other){other.piece = this.piece; other.pieceatt();};
+    edge(board){
+        if(this.x < this.width/2){this.x =this.width/2};
+        if(this.x > board.width-(this.width/2)) {this.x = board.width-(this.width/2)};
+        if(this.y < this.height/2){this.y=this.height/2}
+        if(this.y > board.height-(this.height/2)){this.y = board.height-(this.height/2)};
+    }
+}
 
